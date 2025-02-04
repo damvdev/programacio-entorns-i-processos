a4c9bce4781217b3e26fbfff5e190f1016f3bc8f
damvdev <143938844+damvdev@users.noreply.github.com>
2025-02-04 13:46:00 +0100

    Update Program.cs

--- tema_4/Teoria/Generics & Collections/Program.cs
+++ tema_4/Teoria/Generics & Collections/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 
 namespace colleccions
@@ -7,8 +7,6 @@ namespace colleccions
     {
        public static void Main()
         {
-            /*
-             *
             Console.WriteLine("******************* Array ********************");
             int[] arrayInt = [1,2,3,4,5,6,7,8,9];
             int[] destArray = new int[20];
@@ -40,7 +38,7 @@ namespace colleccions
 
             Caixa<string> caixaText = new Caixa<string>("Hola generics!");
             caixaText.MostrarContingut();
-               */
+              
 
             Console.WriteLine("******************* ArrayList ********************");
             ArrayList arrList = new ArrayList();
@@ -106,18 +104,33 @@ namespace colleccions
             foreach (var item in pila) {
                 Console.WriteLine(item);
             }
-
+            
             Console.WriteLine("******************* List ********************");
             List<Person> llistaPersones = new List<Person>();
             llistaPersones.Add(new Person("Maria", "Garcia"));
             llistaPersones.Add(new Person("Miquel", "Biada"));
+            llistaPersones.Add(new Person("Joan", "Pere"));
             llistaPersones.Sort();
 
             foreach(var item in llistaPersones)
             {
                 Console.WriteLine(item);
             }
+            Console.WriteLine();
+            List<Person> newList = new List<Person>()
+            {
+                new Person("Maria", "Garcia"),
+                new Person("Miquel", "Biada"),
+                new Person("Joan", "Pere")
+            };
 
+            newList.Sort(new PersonComparer());
+
+            foreach(var item in newList)
+            {
+                Console.WriteLine(item);
+            }
+            
             Console.WriteLine("******************* Dictionary ********************");
             Dictionary<int, string> personDict = new Dictionary<int, string>();
             personDict.Add(1, "Maria");
@@ -149,6 +162,44 @@ namespace colleccions
             {
                 Console.WriteLine(item);
             }
+            
+
+            Console.WriteLine("******************* Ordenació amb LINQ ********************");
+            List<int> nums = new List<int> { 4, 1, 8, 3, 5, 2, 7 };
+            nums.Sort();
+            foreach (int x in nums)
+            {
+                Console.Write(x);
+            }
+            Console.WriteLine();
+            Console.WriteLine(string.Join(" | ", nums));
+
+            var llistaOrdenada = nums.OrderBy(n => n).ToList();
+            Console.WriteLine(string.Join(" | ", llistaOrdenada));
+            llistaOrdenada = nums.OrderByDescending(n => n).ToList();
+            Console.WriteLine(string.Join(" | ", llistaOrdenada));
+            
+            Console.WriteLine("******************* Filtratge amb LINQ ********************");
+            List<int> edats = new List<int> { 44, 11, 8, 13, 25, 32, 57, 15, 24, 16 };
+            var majors = edats.Where(e => e >= 25);
+            Console.WriteLine(string.Join(" | ", majors));
+
+            var parells = nums.Where(n => n%2 == 0);
+            Console.WriteLine(string.Join(" | ", parells));
+
+            nums.ForEach(x => Console.WriteLine(x));
+            
+            Console.WriteLine("******************* Dates ********************");
+            DateTime avui = DateTime.Now;
+            Console.WriteLine(avui.ToString("dd/MM/yyyy HH:mm"));
+            DateTime dataPersonalitzada = new DateTime(2025, 3, 5);
+            Console.WriteLine(dataPersonalitzada);
+
+            TimeSpan diferencia = dataPersonalitzada.Subtract(avui);
+            Console.WriteLine(diferencia);
+
+            DateTimeOffset offset = DateTimeOffset.Now;
+            Console.WriteLine(offset);         
         }
     }
 }
