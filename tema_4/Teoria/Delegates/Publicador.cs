a9cb4553aaf46267f5eac8baa90b78c939dc1c01
damvdev <143938844+damvdev@users.noreply.github.com>
2025-02-13 13:57:01 +0100

    Add files via upload

---
+++ tema_4/Teoria/Delegates/Publicador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace colleccions
+{
+    public class Publicador
+    {
+        public delegate void NotificacioEventHandler(string missatge);
+        public event NotificacioEventHandler MissatgeEnviat;
+        public void EnviarMissatge(string missatge) {
+            MissatgeEnviat?.Invoke(missatge);
+        }
+    }
+}
